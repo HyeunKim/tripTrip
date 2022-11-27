f704a9de24c1daee47ca2f4c716b4c819f41b211
HyeunKim <hyeeunkim1230@gmail.com>
2022-11-28 04:29:01 +0900

    add 부분 페이지 정리, overflow 안나게 하는 중, 아직 이미지 추가 버튼 부분 구현 못함
    배고파서 일단 저장하고 먹으러 감

--- lib/add.dart
+++ lib/add.dart
@@ -1,18 +1,18 @@
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import 'home.dart';
-import 'dart:ui';
 import 'guestBook2.dart';
+import 'package:flutter/services.dart';
 
 class AddPage extends StatelessWidget {
   // const ProfilePage({required List providers, required List<SignedOutAction> actions});
-  const AddPage();
+  const AddPage({super.key});
+
 
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual, overlays: []);
     // TODO: Return an AsymmetricView (104)
     // TODO: Pass Category variable to AsymmetricView (104)
     return Scaffold(
@@ -29,20 +29,23 @@ class AddPage extends StatelessWidget {
           ),
 
         ),
-        actions: <Widget>[
-          IconButton(
-            icon: const Icon(
-              Icons.add,
-              semanticLabel: 'add',
-            ),
-            onPressed: () {
-              Navigator.pushNamed(context, '/add');
-              print('add button');
-            },
-          ),
-        ],
+
         iconTheme: const IconThemeData(color: Color(0xFFf8bbd0), size: 35),
       ),
+        floatingActionButton: Container(
+          // alignment: Alignment.center,
+          padding: const EdgeInsets.fromLTRB(100, 0, 150, 0),
+          child: FloatingActionButton(
+            elevation: 0,
+            backgroundColor: const Color(0xFFef9a9a),
+            onPressed: () {
+              flutterDialog(context);
+              // 버튼을 누르면 실행될 코드 작성
+            },
+            child: const Icon(Icons.add),
+          ),
+        ),
+
       drawer: Drawer(
         backgroundColor: const Color(0xffFFCCCC),
         child: Column(
@@ -99,149 +102,224 @@ class AddPage extends StatelessWidget {
               ),
             ),
             // This container holds the align
-            Container(
-              // This align moves the children to the bottom
-                child: Align(
-                    alignment: FractionalOffset.bottomCenter,
-                    // This container holds all the children that will be aligned
-                    // on the bottom and should not scroll with the above ListView
-                    child: Container(
-                        child: Column(
-                          children: const <Widget>[
-                            ListTile(
-                                title: Text('정보수정',style: TextStyle(fontSize: 35,color: Color(0xffff8484),fontWeight: FontWeight.w300))),
-                          ],
-                        )
-                    )
+            Align(
+                alignment: FractionalOffset.bottomCenter,
+                // This container holds all the children that will be aligned
+                // on the bottom and should not scroll with the above ListView
+                child: Column(
+                  children: const <Widget>[
+                    ListTile(
+                        title: Text('정보수정',style: TextStyle(fontSize: 35,color: Color(0xffff8484),fontWeight: FontWeight.w300))),
+                  ],
                 )
             )
           ],
         ),
       ),
-      body: Column(
-        children: [
-          Padding(
-            padding: EdgeInsets.fromLTRB(20, 20, 30, 20),
-            child:
-                Row(
-                  children: [
-                    const Text(
-                      'trip',
-                      style: TextStyle(
-                        // fontFamily: 'Quicksand',
-                        color: Color(0xFFffcdd2),
-                        fontSize: 30,
-                      ),
-                    ),
-                    const Text(
-                      '로그',
-                      style: TextStyle(
-                        // fontFamily: 'Quicksand',
-                        color: Colors.black54,
-                        fontSize: 30,
-                        fontWeight: FontWeight.bold
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.fromLTRB(170, 0, 0, 0),
-                      child: ElevatedButton(
-                          style: ElevatedButton.styleFrom(
-                            backgroundColor: const Color(0xFFffcdd2),
-                            shape:
-                                RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(18.0),
-                            ),
-                          ),
-                          onPressed: () {
-                            // 버튼을 누르면 실행될 코드 작성
-                          },
-                          child: const Text('SAVE')
-                      ),
-                    ),
-
-                  ],
-                ),
-          ),
-
-          Container(
-            // margin: const EdgeInsets.all(10),
-            margin: const EdgeInsets.fromLTRB(10, 0, 10, 20),
-            decoration: BoxDecoration(
-              border: Border.all(
-                width: 1.5,
-                color: Color(0xFFffcdd2),
-              ),
-              borderRadius: const BorderRadius.all(
-                  Radius.circular(15.0) // POINT
-              ),
-            ),
-
-            child: ListView(
-            shrinkWrap: true,
-              children: <Widget>[
-                const SizedBox(height: 8),
-                // const IconAndDetail(Icons.calendar_today, 'October 30'),
-                // const IconAndDetail(Icons.location_city, 'San Francisco'),
-                // Consumer<ApplicationState>(
-                //   builder: (context, appState, _) => AuthFunc(
-                //       loggedIn: appState.loggedIn,
-                //       signOut: () {
-                //         FirebaseAuth.instance.signOut();
-                //       }),
-                // ),
-                // const Divider(
-                //   height: 8,
-                //   thickness: 1,
-                //   indent: 8,
-                //   endIndent: 8,
-                //   color: Colors.grey,
-                // ),
-                // const Header("What we'll be doing"),
-                // const Paragraph(
-                //   'Join us for a day full of Firebase Workshops and Pizza!',
-                // ),
-                Consumer<ApplicationState>(
-                  builder: (context, appState, _) => Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      // if (appState.attendees >= 2)
-                      //   Paragraph('${appState.attendees} people going')
-                      // else if (appState.attendees == 1)
-                      //   const Paragraph('1 person going')
-                      // else
-                      //   const Paragraph('No one going'),
-                      if (appState.loggedIn) ...[
-                        // YesNoSelection(
-                        //   state: appState.attending,
-                        //   onSelection: (attending) => appState.attending = attending,
-                        // ),
-                        // const Text(
-                        //   'trip로그',
-                        //   style: TextStyle(
-                        //     // fontFamily: 'Quicksand',
-                        //     color: Color(0xFFf8bbd0),
-                        //     fontSize: 30,
-                        //   ),
-                        //
-                        // ),
-                        GuestBook2(
-                          addMessage: (title, message) =>
-                              appState.addMessageToGuestBook(title, message),
-                          // addTitle: (title) =>
-                          //     appState.addMessageToGuestBook(title),
+      body:
+      SingleChildScrollView(
+      scrollDirection: Axis.vertical,
+      child:
+        Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.fromLTRB(20, 20, 30, 20),
+              child:
+                  Row(
+                    children: const [
+                      Text(
+                        'trip',
+                        style: TextStyle(
+                          // fontFamily: 'Quicksand',
+                          color: Color(0xFFffcdd2),
+                          fontSize: 30,
                         ),
-                      ],
+                      ),
+                      Text(
+                        '로그',
+                        style: TextStyle(
+                          // fontFamily: 'Quicksand',
+                          color: Colors.black54,
+                          fontSize: 30,
+                          fontWeight: FontWeight.bold
+                        ),
+                      ),
+                      // Padding(
+                      //   padding: const EdgeInsets.fromLTRB(170, 0, 0, 0),
+                      //   child: ElevatedButton(
+                      //       style: ElevatedButton.styleFrom(
+                      //         backgroundColor: const Color(0xFFffcdd2),
+                      //         shape:
+                      //             RoundedRectangleBorder(
+                      //               borderRadius: BorderRadius.circular(18.0),
+                      //         ),
+                      //       ),
+                      //       onPressed: () {
+                      //         // 버튼을 누르면 실행될 코드 작성
+                      //       },
+                      //       child: const Text('SAVE')
+                      //   ),
+                      // ),
+
                     ],
                   ),
-                ),
-              ],
             ),
-          ),
-        ],
+            Container(
+              // height: 500,
+              // margin: const EdgeInsets.all(10),
+              margin: const EdgeInsets.fromLTRB(10, 0, 10, 20),
+              decoration: BoxDecoration(
+                border: Border.all(
+                  width: 1.5,
+                  color: const Color(0xFFffcdd2),
+                ),
+                borderRadius: const BorderRadius.all(
+                    Radius.circular(15.0) // POINT
+                ),
+              ),
+
+              child: ListView(
+              shrinkWrap: true,
+                children: <Widget>[
+                  const SizedBox(height: 8),
+                  // const IconAndDetail(Icons.calendar_today, 'October 30'),
+                  // const IconAndDetail(Icons.location_city, 'San Francisco'),
+                  // Consumer<ApplicationState>(
+                  //   builder: (context, appState, _) => AuthFunc(
+                  //       loggedIn: appState.loggedIn,
+                  //       signOut: () {
+                  //         FirebaseAuth.instance.signOut();
+                  //       }),
+                  // ),
+                  // const Divider(
+                  //   height: 8,
+                  //   thickness: 1,
+                  //   indent: 8,
+                  //   endIndent: 8,
+                  //   color: Colors.grey,
+                  // ),
+                  // const Header("What we'll be doing"),
+                  // const Paragraph(
+                  //   'Join us for a day full of Firebase Workshops and Pizza!',
+                  // ),
+                  Consumer<ApplicationState>(
+                    builder: (context, appState, _) => Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        // if (appState.attendees >= 2)
+                        //   Paragraph('${appState.attendees} people going')
+                        // else if (appState.attendees == 1)
+                        //   const Paragraph('1 person going')
+                        // else
+                        //   const Paragraph('No one going'),
+                        if (appState.loggedIn) ...[
+                          // YesNoSelection(
+                          //   state: appState.attending,
+                          //   onSelection: (attending) => appState.attending = attending,
+                          // ),
+                          // const Text(
+                          //   'trip로그',
+                          //   style: TextStyle(
+                          //     // fontFamily: 'Quicksand',
+                          //     color: Color(0xFFf8bbd0),
+                          //     fontSize: 30,
+                          //   ),
+                          //
+                          // ),
+                          GuestBook2(
+                            addMessage: (title, message) =>
+                                appState.addMessageToGuestBook(title, message),
+                            // addTitle: (title) =>
+                            //     appState.addMessageToGuestBook(title),
+                          ),
+                        ],
+                      ],
+                    ),
+                  ),
+
+                ],
+              ),
+            ),
+
+          ],
+        ),
       ),
 
-
     );
+  }
+
+  void flutterDialog(BuildContext context) {
+    showDialog(
+        context: context,
+        //barrierDismissible - Dialog를 제외한 다른 화면 터치 x
+        // barrierDismissible: false,
+        builder: (BuildContext context) {
+          return SizedBox(
+            width: 50,
+              child: AlertDialog(
+
+                // RoundedRectangleBorder - Dialog 화면 모서리 둥글게 조절
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15.0)),
+                //Dialog Main Title
+                // title: Column(
+                //   children: <Widget>[
+                //     new Text("Dialog Title"),
+                //   ],
+                // ),
+                //
+                content: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Center(
+                      child: TextButton(
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                        style: TextButton.styleFrom(
+                          foregroundColor:  Colors.black54,
+                          textStyle: const TextStyle(fontSize: 20,),
+                        ),
+                        child: const Text("이미지 수정"),
+                      ),
+                    ),
+
+                    const Divider(
+                      height: 8,
+                      thickness: 2,
+                      indent: 8,
+                      endIndent: 8,
+                      color: Color(0xFFffcdd2),
+                    ),
+                    Center(
+                      child: TextButton(
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                        style: TextButton.styleFrom(
+                          foregroundColor:  Colors.black54,
+                          textStyle: const TextStyle(fontSize: 20,),
+                        ),
+                        child: const Text("이미지 삭제"),
+                      ),
+                    ),
+                    // Text(
+                    //   "Dialog Content",
+                    // ),
+                  ],
+                ),
+                // actions: <Widget>[
+                //   TextButton(
+                //     child: new Text("확인"),
+                //     onPressed: () {
+                //       Navigator.pop(context);
+                //     },
+                //   ),
+                // ],
+              ),
+          );
+        });
   }
 
 // @override
