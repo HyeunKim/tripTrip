bb0ba18965866118fea73d9ff3a1af6561bb9256
HyeunKim <hyeeunkim1230@gmail.com>
2022-11-30 04:10:37 +0900

    drawer 위젯 가진 클래스 파일 따로 만들어서 코드 좀 정리함 매번 반복되고 너무 길어서!

--- lib/home.dart
+++ lib/home.dart
@@ -14,6 +14,7 @@ import 'package:pull_to_refresh/pull_to_refresh.dart';
 import 'login.dart';
 import 'src/widgets.dart';
 import 'package:intl/intl.dart';
+import 'drawer.dart';
 
 class HomePage extends StatelessWidget {
   HomePage({super.key});
@@ -56,122 +57,8 @@ class HomePage extends StatelessWidget {
         ],
         iconTheme: const IconThemeData(color: Color(0xFFf8bbd0), size: 35),
       ),
-      drawer: Drawer(
-        backgroundColor: const Color(0xffFFCCCC),
-        child: Column(
-          children: <Widget>[
-            Expanded(
-              // ListView contains a group of widgets that scroll inside the drawer
-              child: ListView(
-                padding: EdgeInsets.zero,
-                children: [
-                  Padding(padding: const EdgeInsets.fromLTRB(35, 60, 0, 50),
-                    child:Align(
-                      alignment: Alignment.topLeft,
-                      child: TextButton(
-                        onPressed: () {
-                          Navigator.pushNamed(context, '/home');
-                        },
-                        style: TextButton.styleFrom(
-                            foregroundColor:  Colors.white,
-                            textStyle: const TextStyle(fontFamily: 'Quicksand', fontSize: 40,color: Colors.white,fontWeight: FontWeight.w100)),
+      drawer: DrawerCustom(),
 
-
-                        child: const Text("tripTrip"),
-                      ),
-                    ),
-                  ),
-
-                  // const DrawerHeader(
-                  //     padding: EdgeInsets.fromLTRB(30, 30, 0, 0),
-                  //     child: Text("tripTrip",
-                  //         style: TextStyle(fontFamily: 'Quicksand', fontSize: 50,color: Colors.white,fontWeight: FontWeight.w100))
-                  // ),
-                  Padding(padding: const EdgeInsets.only(left: 40),
-                      child:ListTile(
-                        minLeadingWidth: 20,
-                        leading: const Text('/',style: TextStyle(fontFamily: 'Quicksand',fontSize: 40,color: Colors.white,fontWeight: FontWeight.w100)),
-                        title: const Text("MY",style: TextStyle(fontSize: 25,color: Color(0xffff8484),fontWeight: FontWeight.w300 ),),
-                        onTap:(){
-                          if(FirebaseAuth.instance.currentUser==null){
-                            Navigator.pushNamed(context, '/sign-in');
-                          }else{
-                            FirebaseAuth.instance.signOut();
-                            if(FirebaseAuth.instance.currentUser==null){
-                              Navigator.pushNamed(context, '/sign-in');
-                            }
-                            // Navigator.popUntil(context, ModalRoute.withName('/sign-in'));//Navigator.pushNamed(context, '/MY');
-                            // _signOut();
-                            // Navigator.popUntil(context, ModalRoute.withName('/sign-in'));//Navigator.pushNamed(context, '/MY');
-                          }
-                        },
-                      )
-                  ),
-                  Padding(padding: const EdgeInsets.only(left: 40),
-                      child:ListTile(
-                        minLeadingWidth: 20,
-                        leading: const Text('/',style: TextStyle(fontFamily: 'Quicksand',fontSize: 40,color: Colors.white,fontWeight: FontWeight.w100)),
-                        title: RichText(text: const TextSpan(text: "trip",
-                            style: TextStyle(fontSize: 25,color: Color(0xffff8484),fontWeight: FontWeight.w300),
-                            children: <TextSpan>[TextSpan(text: '앨범', style: TextStyle(fontSize: 25,color: Colors.white,fontWeight: FontWeight.w300))])
-                        ),
-                        onTap: (){
-                          Navigator.pushNamed(context, '/sign-in');
-                        },
-                      )
-                  ),
-                  Padding(padding: const EdgeInsets.only(left: 40),
-                      child:ListTile(
-                        minLeadingWidth: 20,
-                        leading: const Text('/',style: TextStyle(fontFamily: 'Quicksand',fontSize: 40,color: Colors.white,fontWeight: FontWeight.w100)),
-                        title: RichText(text: const TextSpan(text: "trip",
-                            style: TextStyle(fontSize: 25,color: Color(0xffff8484),fontWeight: FontWeight.w300),
-                            children: <TextSpan>[TextSpan(text: '로그', style: TextStyle(fontSize: 25,color: Colors.white,fontWeight: FontWeight.w300))])
-                        ),
-                        onTap: (){
-                          Navigator.pushNamed(context, '/home');
-                        },
-                      )
-                  ),
-                  Padding(padding: const EdgeInsets.only(left: 40),
-                      child:ListTile(
-                        minLeadingWidth: 20,
-                        leading: const Text('/',style: TextStyle(fontFamily: 'Quicksand',fontSize: 40,color: Colors.white,fontWeight: FontWeight.w100)),
-                        title: RichText(text: const TextSpan(text: "trip",
-                            style: TextStyle(fontSize: 25,color: Color(0xffff8484),fontWeight: FontWeight.w300),
-                            children: <TextSpan>[TextSpan(text: '코인', style: TextStyle(fontSize: 25,color: Colors.white,fontWeight: FontWeight.w300))])
-                        ),
-                        onTap: (){
-                          Navigator.pushNamed(context, '/');
-                        },
-                      )
-                  ),
-                ],
-              ),
-            ),
-            // This container holds the align
-            Container(
-              // This align moves the children to the bottom
-                child: Align(
-                    alignment: FractionalOffset.bottomCenter,
-                    // This container holds all the children that will be aligned
-                    // on the bottom and should not scroll with the above ListView
-                    child: Container(
-                        child: Column(
-                          children: const <Widget>[
-                            Padding(
-                                padding: EdgeInsets.fromLTRB(15, 0, 0, 15),
-                                child: ListTile(
-                                    title: Text('정보수정',style: TextStyle(fontSize: 25,color: Color(0xffff8484),fontWeight: FontWeight.w300))),
-                            )
-                          ],
-                        )
-                    )
-                )
-            )
-          ],
-        ),
-      ),
         floatingActionButton: Container(
           // alignment: Alignment.center,
           padding: const EdgeInsets.fromLTRB(100, 0, 150, 0),
@@ -311,13 +198,6 @@ class HomePage extends StatelessWidget {
               // RoundedRectangleBorder - Dialog 화면 모서리 둥글게 조절
               shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(15.0)),
-              //Dialog Main Title
-              // title: Column(
-              //   children: <Widget>[
-              //     new Text("Dialog Title"),
-              //   ],
-              // ),
-              //
               content: Column(
                 mainAxisSize: MainAxisSize.min,
                 crossAxisAlignment: CrossAxisAlignment.start,
@@ -354,19 +234,8 @@ class HomePage extends StatelessWidget {
                       child: const Text("로그 추가하기"),
                     ),
                   ),
-                  // Text(
-                  //   "Dialog Content",
-                  // ),
                 ],
               ),
-              // actions: <Widget>[
-              //   TextButton(
-              //     child: new Text("확인"),
-              //     onPressed: () {
-              //       Navigator.pop(context);
-              //     },
-              //   ),
-              // ],
             ),
           );
         });
